9a7d4ac37623bbd368f2aa1a175d3c63f5ead34b
youngjae459 <youngjae459@gmail.com>
2023-04-18 22:45:58 +0900

    feat: @ResponseBod를 사용해 url매핑

--- src/main/java/com/yj/sbbasic1/MainController.java
+++ src/main/java/com/yj/sbbasic1/MainController.java
@@ -1,4 +1,14 @@
 package com.yj.sbbasic1;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class MainController {
+    @GetMapping("/sbb")
+    public String index() {
+        return "안녕하세요 영재가 만든 사이트에 오신것을 환영합니다.";
+    }
 }
