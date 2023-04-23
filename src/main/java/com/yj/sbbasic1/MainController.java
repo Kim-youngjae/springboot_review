15f242cf8a92550ff8142eef7ae530b31558ddfb
youngjae459 <youngjae459@gmail.com>
2023-04-23 16:56:11 +0900

    컨트롤러 추가 및 템플릿 적용

--- src/main/java/com/yj/sbbasic1/MainController.java
+++ src/main/java/com/yj/sbbasic1/MainController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 public class MainController {
     @GetMapping("/sbb")
     public String index() {
         return "안녕하세요 영재가 만든 사이트에 오신것을 환영합니다.";
     }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+
 }
