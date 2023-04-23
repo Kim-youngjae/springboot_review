081a9f4730ae00e79f91b037f155fbbac57a9508
youngjae459 <youngjae459@gmail.com>
2023-04-23 15:59:53 +0900

    리포지터리 구현 및 테스트

--- src/main/java/com/yj/sbbasic1/QuestionRepository.java
+++ src/main/java/com/yj/sbbasic1/QuestionRepository.java
@@ -1,7 +1,14 @@
 package com.yj.sbbasic1;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Optional<Question> findBySubject(String subject);
 
+    Optional<Question> findBySubjectAndContent(String subject, String content);
 }
--- src/test/java/com/yj/sbbasic1/Sbbasic1ApplicationTests.java
+++ src/test/java/com/yj/sbbasic1/Sbbasic1ApplicationTests.java
@@ -1,21 +1,33 @@
 package com.yj.sbbasic1;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.MethodName.class)
 class Sbbasic1ApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
 
+	@Autowired
+	private AnswerRepository answerRepository;
+
 	@Test
-	@DisplayName("리포지터리 테스트")
+	@DisplayName("1: 리포지터리 테스트")
 	void t001() {
+
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
@@ -27,5 +39,118 @@ class Sbbasic1ApplicationTests {
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
 		this.questionRepository.save(q2);  // 두번째 질문 저장
+	}
+
+	@Test
+	@DisplayName("2: DB에 저장된 데이터 조회")
+	void t002() {
+		List<Question> questionList = this.questionRepository.findAll();
+
+		assertThat(questionList.size()).isEqualTo(2);
+
+		assertThat(questionList.get(0).getSubject()).isEqualTo("sbb가 무엇인가요?");
+	}
+
+	@Test
+	@DisplayName("3: id값으로 데이터 조회")
+	void t003() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+
+		if (oq.isPresent()) {
+			Question question = oq.get();
+			assertThat(question.getSubject()).isEqualTo("sbb가 무엇인가요?");
+		}
+	}
+
+	@Test
+	@DisplayName("4: findBySubject() ")
+	void t004() {
+		Optional<Question> oq = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+
+		if (oq.isPresent()) {
+			Question question = oq.get();
+
+			assertThat(question.getContent()).isEqualTo("sbb에 대해서 알고 싶습니다.");
+		}
+	}
+
+	@Test
+	@DisplayName("5: findBySubjectAndContent()")
+	void t005() {
+		Optional<Question> oq = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+
+		if (oq.isPresent()) {
+			Question question = oq.get();
+
+			assertThat(question.getId()).isEqualTo(1);
+		}
+	}
+
+	@Test
+	@DisplayName("6: 데이터 수정하기")
+	void t006() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+
+		if (oq.isPresent()) {
+			Question question = oq.get();
+
+			question.setSubject("수정된 제목");
+
+			this.questionRepository.save(question);
+
+			assertThat(question.getContent()).isEqualTo("sbb에 대해서 알고 싶습니다.");
+		}
+	}
+
+	@Test
+	@DisplayName("7: 데이터 삭제하기")
+	void t007() {
+		long count = this.questionRepository.count();
+
+		assertThat(count).isEqualTo(2);
+
+		Optional<Question> oq = this.questionRepository.findById(1);
+
+		if (oq.isPresent()) {
+			Question question = oq.get();
+
+			this.questionRepository.delete(question);
+
+			assertThat(this.questionRepository.count()).isEqualTo(1);
+		}
+	}
+
+	@Test
+	@DisplayName("8: 답변 데이터 생성하기")
+	void t008() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+
+		if (oq.isPresent()) {
+			Question question = oq.get();
+
+			Answer a = new Answer();
+
+			a.setContent("네 자동으로 생성됩니다.");
+			a.setQuestion(question);
+			a.setCreateDate(LocalDateTime.now());
+			this.answerRepository.save(a);
+		}
+	}
+
+	@Transactional
+	@Test
+	@DisplayName("9: 답변에 연결된 질문 찾기")
+	void t009() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+
+		if (oq.isPresent()) {
+			Question q = oq.get();
+
+			List<Answer> answerList = q.getAnswerList();
+
+			assertThat(answerList.size()).isEqualTo(1);
+
+			assertThat(answerList.get(0)).isEqualTo("네 자동으로 생성됩니다.");
+		}
 	}
 }
